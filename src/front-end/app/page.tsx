2d7d7110eafe8b8e2c49e80cdc3b7627f48314fa
franco <ougaoyuan0718@gmail.com>
2024-03-27 10:43:23 +1100

    front-end initialsation

---
+++ src/front-end/app/page.tsx
@@ -0,0 +1,12 @@
+import {Home} from "@/components/home";
+
+function Page() {
+
+    return (
+        <div>
+            <Home/>
+        </div>
+    );
+}
+
+export default Page;
---
+++ src/front-end/components/home.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+
+import {useEffect, useState} from "react";
+import {Simulate} from "react-dom/test-utils";
+
+export const Home = () => {
+    const [text, setText] = useState('Loading');
+    useEffect(() => {
+        fetch("http://localhost:8080/api/home").then(
+            response => response.json()
+        ).then(
+            data => {
+                console.log(data)
+                setText(data.message)
+            }
+        ).catch(error => console.log(error));
+    }, []);
+
+    return (
+        <div>
+            {text}
+        </div>
+    )
+}
