309dc57634328f14752c31e07badc029cb09fa57
franco <ougaoyuan0718@gmail.com>
2024-04-01 15:25:09 +1100

    logo added

--- src/front-end/app/(dashboard)/_component/sidebar.tsx
+++ src/front-end/app/(dashboard)/_component/sidebar.tsx
@@ -1,10 +1,11 @@
 import { SidebarRoutes } from "./sidebar-routes"
+import {Logo} from "./logo";
 
 export const Sidebar = () => {
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
       <div className="p-6">
-        {/*<Logo />*/}
+        <Logo />
       </div>
       <div className="flex flex-col w-full">
         <SidebarRoutes />
