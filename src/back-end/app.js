405507cf03e8ffb2054a3293f35993d9ae5d0867
Holt <holtwyw@hotmail.com>
2024-04-11 15:20:54 +1000

    Backend code structure
    1. Include server.js and app.js
    2. Include express and mongoose packages

---
+++ src/back-end/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+// TODO: Implement middlewares
+
+// TODO: Implement routers
+// app.use("/users", userRouter);
+
+module.exports = app;
