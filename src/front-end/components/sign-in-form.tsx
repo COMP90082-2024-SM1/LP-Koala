8c9c9ca870092778a939615262e6b232aa5b7398
franco <ougaoyuan0718@gmail.com>
2024-04-14 13:25:23 +1000

    login integrated

--- src/front-end/components/sign-in-form.tsx
+++ src/front-end/components/sign-in-form.tsx
@@ -2,23 +2,45 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import {useRouter} from "next/navigation";
-import axios from "axios";
-import React, {MouseEventHandler} from "react";
+import React, { useState} from "react";
+import {ClipLoader} from "react-spinners";
+import Cookies from 'js-cookie';
 
 const SignInForm = () => {
-
     const router = useRouter()
-
+    const [isErrorMessageDisplayed, setErrorMessageDisplayed] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [formData, setFormData] = useState({username:'', password:''});
     const onClick = async (e: React.FormEvent) => {
-        e.preventDefault()
-        const response = await axios.post('http://localhost:8081/api/log-in',{name: 'franco', password:'nihao'})
-        console.log(response)
-        if (response.status === 200) {
+        e.preventDefault();
+        setIsLoading(true);
+        setErrorMessageDisplayed(false);
+        try {
+            const response = await fetch('http://localhost:3000/users/login', {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+            setErrorMessageDisplayed(false);
+            const data = await response.json();
+            Cookies.set('token','Bearer' + data.token);
+            Cookies.set('userId', data.data.user._id);
+            console.log(Cookies.get('token'));
             router.push('/projects');
+        }catch(error) {
+            // console.log(error)
+            setIsLoading(false)
+            setErrorMessageDisplayed(true)
+            setFormData((prevState) => ({...prevState, password: ''}))
         }
     }
 
-
+    const onChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+        const {name,value} = event.target;
+        setFormData((prevState)=> ({...prevState, [name]:value}))
+    }
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="mx-auto w-full max-w-lg">
@@ -36,22 +58,27 @@ const SignInForm = () => {
                 <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                     Sign in to your account
                 </h2>
+                <p className='text-red-600 my-4' hidden={!isErrorMessageDisplayed}>
+                    Incorrect Username or Password
+                </p>
             </div>
 
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <form className="space-y-6" onSubmit={onClick} action="#" method="POST">
                     <div>
                         <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                            Email address
+                            Username
                         </label>
                         <div className="mt-2">
                             <input
-                                id="email"
-                                name="email"
-                                type="email"
-                                autoComplete="email"
+                                id="username"
+                                name="username"
+                                type="usernane"
+                                autoComplete="username"
                                 required
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                value={formData.username}
+                                onChange={onChange}
                             />
                         </div>
                     </div>
@@ -70,15 +97,19 @@ const SignInForm = () => {
                                 autoComplete="current-password"
                                 required
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                value={formData.password}
+                                onChange={onChange}
                             />
                         </div>
                     </div>
 
                     <div>
                         <Button
+                            disabled={isLoading}
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-[#1c407f] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
+                            <ClipLoader hidden={!isLoading} loading={isLoading} size={20} className='!border-sky-700 !border-b-transparent'/>
                             Sign in
                         </Button>
                     </div>
