b0d53023a54b6c3d1a3ee5cb7d25c7393ac6e5ad
Deedead <1991110093@qq.com>
2024-04-21 00:24:32 +1000

    activity update V3

--- src/back-end/controllers/activityController.js
+++ src/back-end/controllers/activityController.js
@@ -1,9 +1,12 @@
 const catchAsync = require('./../utils/asyncCatch');
 const {singleChoice, mcqQuestion} = require('./../models/multipleChoiceModel');
+const activity = require('./../models/activityModel');
 const factory = require('./activityHandler');
 
 
 exports.getAllChoice = factory.getAll(singleChoice);
 exports.getAllMcqQuestions = factory.getAll(mcqQuestion);
+exports.getAllActivity = factory.getAll(activity);
 exports.createChoice = factory.createOne(singleChoice);
 exports.createMcq = factory.createOne(mcqQuestion);
+exports.createActivity = factory.createOne(activity);
