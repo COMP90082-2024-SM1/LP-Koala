03e88c3bfbfbd7893623787854f54f07c7b4aa51
Deedead <1991110093@qq.com>
2024-04-20 21:17:19 +1000

    MCQ updateV1

---
+++ src/back-end/models/multipleChoiceModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const mcqSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: [true, 'A question must have a name!']
+    },
+    choices : [{
+        type: mongoose.Schema.ObjectId,
+        ref: 'singleChoices'
+    }]
+})
+
+const singleChoiceSchema = new mongoose.Schema({
+    text :{
+        type: String,
+        required: [true, 'A choice can not be empty!']
+    },
+    isCorrect : {
+        type: Boolean,
+        required: [true, 'A choice must have answer!']
+    }
+})
+
+const singleChoice = mongoose.model('singleChoices', singleChoiceSchema);
+const mcqQuestion = mongoose.model('multipleChoices', mcqSchema);
