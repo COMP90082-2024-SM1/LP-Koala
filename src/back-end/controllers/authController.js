1771ced1f51b21a1bc6a76b6413606e800e05547
franco <ougaoyuan0718@gmail.com>
2024-04-21 21:04:38 +1000

    merged backend

--- src/back-end/controllers/authController.js
+++ src/back-end/controllers/authController.js
@@ -50,7 +50,6 @@ exports.protect = asyncCatch(async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-  console.log(token)
   if (!token) {
     return next(new AppError('Please log in to get access.', 401));
   }
