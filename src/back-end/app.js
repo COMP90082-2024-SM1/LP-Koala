9bd7443e6d48019ffbd88f09a0276c317199c917
qinglz <qinglinz@student.unimelb.edu.au>
2024-04-22 13:21:07 +1000

    trigger

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const authRouter = require("./routes/authRoute");
-const userRouter = require("./routes/userRoute");
-const projectRouter = require("./routes/projectRoute");
-const cors = require("cors");
+const express = require('express');
+const authRouter = require('./routes/authRoute');
+const userRouter = require('./routes/userRoute');
+const projectRouter = require('./routes/projectRoute');
+const cors = require('cors');
 
 const app = express();
 
+let trigger = 'I want to trigger test';
 // TODO: Implement middlewares
 app.use(express.json());
 app.use((req, res, next) => {
@@ -14,8 +15,8 @@ app.use((req, res, next) => {
 });
 app.use(cors());
 // TODO: Implement routers
-app.use("/users", userRouter);
-app.use("/projects", projectRouter);
+app.use('/users', userRouter);
+app.use('/projects', projectRouter);
 // TODO: Implement global error handler here
 //app.use(globalErrorHandler);
 
