1ec8c5baaa66e11f7cb2103567fdad1437b34539
haofengc <haofengc@student.unimelb.edu.au>
2024-04-26 16:54:17 +1000

    fix module page bug

--- src/front-end/app/projects/[projectId]/page.tsx
+++ src/front-end/app/projects/[projectId]/page.tsx
@@ -1,27 +1,55 @@
+"use client";
+import React, { useEffect, useState } from 'react';
 import {redirect} from "next/navigation";
-import {cookies} from "next/headers";
+import Cookies from "js-cookie";
 
+interface Module {
+    _id: string;
+    title: string;
+  }
 
 interface ProjectProps  {
     params: {projectId: string}
 }
 
 async function Page({params}:ProjectProps) {
-    const token = cookies().get('token')?.value
+    const [firstModuleId, setFirstModuleId] = useState<string | null>(null);
 
-    const response = await fetch(`http://localhost:3000/projects/${params.projectId}`, {
-            method: "GET",
-            headers: {
-                "Authorization": token!
+    useEffect(() => {
+        const fetchFirstModule = async () => {
+          try {
+            const response = await fetch(`http://localhost:3000/projects/${params}/modules`, {
+              method: 'GET',
+              headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                'Authorization': Cookies.get('token')!
+              }
+            });
+            if (!response.ok) {
+              throw new Error('Failed to fetch modules');
             }
-        });
-    const {data: {data}} = await response.json();
+            const responseObject = await response.json();
+            const modules = responseObject.data as Module[];
+            if (modules.length > 0) {
+              setFirstModuleId(modules[0]._id);  // Save the first module ID
+            } else {
+              console.log("No modules found");
+            }
+          } catch (error) {
+            console.error('Error fetching modules:', error);
+          }
+        };
+    
+        fetchFirstModule();
+    }, [params]);
 
-    if (!data){
-        redirect('/projects')
-    }
+    useEffect(() => {
+        if (firstModuleId) {
+            redirect(`/projects/${params.projectId}/modules/${firstModuleId}`)
+        }
+      }, [firstModuleId]);
 
-    redirect(`/projects/${params.projectId}/modules/${data.modules[0]}`)
+    
 
 }
 
