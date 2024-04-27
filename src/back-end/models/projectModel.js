fbe4d3d4fd2ab1ba6553f36174af4a526f13749b
Deedead <1991110093@qq.com>
2024-04-27 16:49:08 +1000

    file fix

--- src/back-end/models/projectModel.js
+++ src/back-end/models/projectModel.js
@@ -13,19 +13,12 @@ const projectSchema = new Item({
   image: {
     type: String,
   },
-<<<<<<< HEAD
   modules: [
     {
       type: mongoose.Schema.ObjectId,
       ref: 'Module',
     },
   ],
-=======
-  // modules: {
-  //   type: mongoose.Schema.ObjectId,
-  //   ref: 'Module',
-  // },
->>>>>>> 2d0f1f45186af4d87ef9d46e91858e97dcef130f
 });
 const Project = mongoose.model('Project', projectSchema);
 
