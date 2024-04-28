f48162ffb8bb42ca1a8d773341ff54b5638cf3dc
Deedead <1991110093@qq.com>
2024-04-28 19:21:30 +1000

    deployment update

--- src/back-end/utils/asyncCatch.js
+++
@@ -1,7 +0,0 @@
-// Take a callback function, add a catching mechanism,
-// and encapsulate all to a function that is to be returned.
-module.exports = (f) => {
-  return (req, res, next) => {
-    f(req, res, next).catch(next);
-  };
-};
