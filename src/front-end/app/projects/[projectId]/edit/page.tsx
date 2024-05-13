2f5b03d07e6ee2be1709c98b215914f10150b99b
franco <ougaoyuan0718@gmail.com>
2024-05-13 15:22:51 +1000

    correct the request url in project edit page

--- src/front-end/app/projects/[projectId]/edit/page.tsx
+++ src/front-end/app/projects/[projectId]/edit/page.tsx
@@ -63,7 +63,7 @@ const EditPage: React.FC<PageProps> = ({params}) =>{
 
   useEffect(() => {
     const fetchProject = async () => {
-      const response = await fetch(`https://lp-koala-backend-c0a69db0f618.herokuapp.com/${params.projectId}`, {
+      const response = await fetch(`https://lp-koala-backend-c0a69db0f618.herokuapp.com/projects/${params.projectId}`, {
         method: 'GET',
         headers: {
             "Content-type": "application/json; charset=UTF-8",
