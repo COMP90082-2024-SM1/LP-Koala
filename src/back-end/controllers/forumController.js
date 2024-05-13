a3870ef1236fe85a13beb24bfa2e738167f6f87a
qinglz <qinglinz@student.unimelb.edu.au>
2024-05-13 20:06:37 +1000

    forum api update

--- src/back-end/controllers/forumController.js
+++ src/back-end/controllers/forumController.js
@@ -6,7 +6,74 @@ const Project = require('./../models/projectModel');
 const factory = require('./activityHandler');
 const asyncCatch = require('./../utils/asyncCatch');
 const AppError = require('./../utils/appError');
+const mongoose = require('mongoose');
 
+const isDescendent = async (rootId, descendantType, descendantId) => {
+  const aggconfig = [
+    { $match: { _id: rootId } },
+    // Lookup Threads
+    {
+      $lookup: {
+        from: 'threads',
+        localField: 'threads',
+        foreignField: '_id',
+        as: 'threads',
+      },
+    },
+  ];
+  console.log(descendantType);
+  console.log(rootId);
+  console.log(descendantId);
+  if (descendantType === 'thread') {
+    aggconfig.push({ $match: { 'threads._id': descendantId } });
+  } else if (descendantType === 'post') {
+    aggconfig.push(
+      ...[
+        { $unwind: '$threads' },
+        {
+          $lookup: {
+            from: 'posts',
+            localField: 'threads.posts',
+            foreignField: '_id',
+            as: 'posts',
+          },
+        },
+        { $match: { 'posts._id': descendantId } },
+      ]
+    );
+  } else if (descendantType === 'respond') {
+    aggconfig.push(
+      ...[
+        { $unwind: '$threads' },
+        {
+          $lookup: {
+            from: 'posts',
+            localField: 'threads.posts',
+            foreignField: '_id',
+            as: 'posts',
+          },
+        },
+        { $unwind: '$posts' },
+        { $match: { 'posts.responds': descendantId } },
+      ]
+    );
+  } else {
+    return false;
+  }
+  aggconfig.push(
+    ...[{ $project: { _id: 1, exists: { $literal: true } } }, { $limit: 1 }]
+  );
+  console.log(aggconfig);
+  const result = await Forum.aggregate(aggconfig);
+  console.log(result);
+  if (result.length === 0) {
+    console.log(`Instance is not in the given grandparent`);
+    return false;
+  } else {
+    console.log(`Instance is in the given grandparent`);
+    return true;
+  }
+};
 const createChild = (Model) =>
   asyncCatch(async (req, res, next) => {
     let parent;
@@ -22,6 +89,7 @@ const createChild = (Model) =>
         new AppError('No such parent document found with given ID', 404)
       );
     }
+    req.body.user = req.user.id;
     const newInstance = await Model.create(req.body);
     parent[Model.modelName.toLowerCase() + 's'].push(newInstance._id);
     parent.save();
@@ -32,6 +100,33 @@ const createChild = (Model) =>
         data: newInstance,
       },
     });
+  });
+
+exports.checkDescendant = (Model) =>
+  asyncCatch(async (req, res, next) => {
+    const rootProject = await Project.findById(req.params.projectId);
+    if (Model.modelName === 'Forum') {
+      if (req.params.id != rootProject.forum) {
+        return next(
+          new AppError("This project doesn't have this document", 404)
+        );
+      }
+      next();
+    } else {
+      const forumID = rootProject.forum;
+      const descendantId = new mongoose.Types.ObjectId(req.params.id);
+      const result = await isDescendent(
+        forumID,
+        Model.modelName.toLowerCase(),
+        descendantId
+      );
+      if (!result) {
+        return next(
+          new AppError("This project doesn't have this document", 404)
+        );
+      }
+      next();
+    }
   });
 
 exports.getAllForums = factory.getAll(Forum);
--- src/back-end/routes/forumRoute.js
+++ src/back-end/routes/forumRoute.js
@@ -1,44 +1,53 @@
 const express = require('express');
 const forumController = require('./../controllers/forumController');
-const {
-  protect,
-  restrictTo,
-  checkAccess,
-} = require('../controllers/authController');
+const Respond = require('../models/forumModels/respondModel');
+const Post = require('../models/forumModels/postModel');
+const Thread = require('../models/forumModels/threadModel');
+const Forum = require('../models/forumModels/forumModel');
 const router = express.Router({ mergeParams: true });
 
-router.use(protect);
+router.route('/').post(forumController.createOneForum);
 
-router
-  .route('/')
-  .get(forumController.getAllForums)
-  .post(forumController.createOneForum);
+// router.route('/threads/').get(forumController.getAllThreads);
 
-router.route('/threads/').get(forumController.getAllThreads);
+// router.route('/threads/posts/').get(forumController.getAllPosts);
 
-router.route('/threads/posts/').get(forumController.getAllPosts);
-
-router.route('/threads/posts/responds/').get(forumController.getAllResponds);
+// router.route('/threads/posts/responds/').get(forumController.getAllResponds);
 
 router
   .route('/threads/posts/responds/:id')
-  .get(forumController.getOneRespond)
-  .delete(forumController.deleteOneRespond);
+  .get(forumController.checkDescendant(Respond), forumController.getOneRespond)
+  .delete(
+    forumController.checkDescendant(Respond),
+    forumController.deleteOneRespond
+  );
 router
   .route('/threads/posts/:id')
-  .get(forumController.getOnePost)
-  .delete(forumController.deleteOnePost)
-  .post(forumController.createOneRespond);
+  .get(forumController.checkDescendant(Post), forumController.getOnePost)
+  .delete(forumController.checkDescendant(Post), forumController.deleteOnePost)
+  .post(
+    forumController.checkDescendant(Post),
+    forumController.createOneRespond
+  );
 
 router
   .route('/threads/:id')
-  .get(forumController.getOneThread)
-  .delete(forumController.deleteOneThread)
-  .post(forumController.createOnePost);
+  .get(forumController.checkDescendant(Thread), forumController.getOneThread)
+  .delete(
+    forumController.checkDescendant(Thread),
+    forumController.deleteOneThread
+  )
+  .post(forumController.checkDescendant(Thread), forumController.createOnePost);
 router
   .route('/:id')
-  .get(forumController.getOneForum)
-  .delete(forumController.deleteOneForum)
-  .post(forumController.createOneThread);
+  .get(forumController.checkDescendant(Forum), forumController.getOneForum)
+  .delete(
+    forumController.checkDescendant(Forum),
+    forumController.deleteOneForum
+  )
+  .post(
+    forumController.checkDescendant(Forum),
+    forumController.createOneThread
+  );
 
 module.exports = router;
